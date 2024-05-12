cf8a4aade2fdc5b386103d0610c06e79212b4bae
welintonborges <welinton.b.borges@gmail>
2024-05-12 11:21:46 -0300

    começo  dos estudo em um projeto vue 03

---
+++ src/App.vue
@@ -0,0 +1,20 @@
+<script >
+
+import {defineComponent} from "vue";
+import BarraLateral from "@/components/BarraLateral.vue";
+import Formulario  from "@/components/Formulario.vue";
+export default defineComponent({
+  components: {Formulario, BarraLateral}
+})
+</script>
+
+<template>
+      <main class="columns is-gapless is-multiline">
+        <div class="column is-one-quarter">
+          <BarraLateral/>
+        </div>
+        <div class="column is-three-quarter">
+        <formulario/>
+        </div>
+      </main>
+</template>
---
+++ src/components/Formulario.vue
@@ -0,0 +1,39 @@
+<script  lang="ts">
+import {defineComponent} from "vue";
+export default defineComponent({
+  name: "Formulario",
+})
+</script>
+
+<template>
+  <div class="box">
+    <div class="columns">
+      <div class="column is-8" role="form" aria-labelledby="formulario pra criação de npvas tarefas">
+        <input type="text" class="input" placeholder="Qual tarefa você deseja iniciar"/>
+      </div>
+      <div class="column">
+        <div class="is-flex is-alingn-items-center is-justify-content-space-between">
+          <section>
+            <strong>00:00:00</strong>
+          </section>
+          <button class="button">
+              <span class="icon">
+                  <i class="fas fa-play"></i>
+              </span>
+            <span>play</span>
+          </button>
+          <button class="button">
+              <span class="icon">
+                  <i class="fas fa-stop"></i>
+              </span>
+            <span>stop</span>
+          </button>
+        </div>
+      </div>
+    </div>
+  </div>
+</template>
+
+<style scoped>
+
+</style>
